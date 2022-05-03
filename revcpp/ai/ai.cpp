8ddae371502539e6d1aeb0b7537f0639cecd6e04
junkanai <kanai.jun.1020@gmail.com>
2022-05-04 01:37:46 +0900

    create AI::birth

--- revcpp/ai/ai.cpp
+++ revcpp/ai/ai.cpp
@@ -1,1 +1,14 @@
 #include "ai.h"
+
+AI AI::birth(AI& father) {
+	AI child;
+	for ( int i=0; i<PARAMETERS; i++ ) {
+		for ( int j=0; j<PARAMETERS; j++ ) {
+			//if ( roulette(0.6) ) child.i_to_m[i][j] = i_to_m[i][j];
+			//else child.i_to_m[i][j] = father.i_to_m[i][j];
+		}
+		//if ( roulette(0.6) ) child.m_to_o[i] = m_to_o[i];
+		//else child.m_to_o[i] = father.m_to_o[i];
+	}
+	return child;
+}
